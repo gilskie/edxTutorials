46894105fe54c911d5884f15036358525361cbfa
gil cano <48949318+gilskie@users.noreply.github.com>
2019-10-01 10:59:14 +0800

    new assignment for word count on edx website

---
+++ gettingDataTutorial.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def main():
+    sample_url = "https://en.wikipedia.org/wiki/main_page"
+    search_string2 = "Did you know"
+    search_string1 = "Did you kn0w"
+
+    response = requests.get(sample_url)
+    print(f"Info: Accessing '{sample_url}'")
+
+    if response.status_code == 200:
+        print(f"Info: Access confirmed! Checking for search string '{search_string1}'")
+        # print(response.text.find(search_string))
+        if response.text.find(search_string1) > 0:
+            print(f"Info: Success! '{search_string1}' could be found at '{sample_url}'.")
+        else:
+            print(f"Error: Unable to find '{search_string1}'  at '{sample_url}'")
+    else:
+        print(f"Error: URL '{sample_url}' could not be found!")
+
+main()
